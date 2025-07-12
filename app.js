f1a8f3a2ca51afa01882f03ce9ca1a8a146f2bf8
Ashwani Yadav <yadav8800766165@gmail.com>
2025-07-12 21:21:07 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const ejs=require("ejs");
 
 const env=require('dotenv');
 env.config();
-const PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT || 5000;
 
 const server=http.createServer(app);
 
